d2a5d9d022e6ede671dbc9afad0d7a6c91f9cbe8
himaja gattu <himajagattu@himajas-MBP.hsd1.ca.comcast.net>
2018-03-05 21:47:57 -0800

    adding files

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,24 @@
 var mysql = require("mysql");
-var connection = mysql.createConnection({
+
+var connection;
+if(process.env.JAWSDB_URL) {
+  //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //local host
+    connection = mysql.createConnection({
+        root: 3000,
+        host: "localhost",
+        user: "root",
+        password: "hima",
+        database: "burgers_db",
+    });
+};
+
+
+
+
+/*var connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "root",
@@ -14,5 +33,5 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
-// Export connection for our ORM to use.
+// Export connection for our ORM to use.*/
 module.exports = connection;
